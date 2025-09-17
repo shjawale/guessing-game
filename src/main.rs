facb71c547a493301d68418f98953dfadaa5655c
shruti <shruti.s.jawale@gmail.com>
2025-09-17 13:53:36 -0700

    modified main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess");
 
-    let mut input = String::new();
+    let mut guess = String::new();
+
     let stdin = io::stdin();
-    stdin.read_line(&mut input);
-    println!("{input}");
-    
-    
+    stdin.read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
